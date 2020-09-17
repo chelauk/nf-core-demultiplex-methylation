c5c44b45b19a1ad4f043359f017a240857a57b08
Chela James <chela.james@icr.ac.uk>
2020-09-17 11:56:28 +0100

    bin/scrape_software_versions.py

--- bin/scrape_software_versions.py
+++ bin/scrape_software_versions.py
@@ -7,14 +7,18 @@ import re
 regexes = {
     'nf-core/demultiplex': ['v_pipeline.txt', r"(\S+)"],
     'Nextflow': ['v_nextflow.txt', r"(\S+)"],
-    'FastQC': ['v_fastqc.txt', r"FastQC v(\S+)"],
-    'MultiQC': ['v_multiqc.txt', r"multiqc, version (\S+)"],
+    'FastQC'  : ['v_fastqc.txt', r"FastQC v(\S+)"],
+    'MultiQC' : ['v_multiqc.txt', r"multiqc, version (\S+)"],
+    'Bismark' : ['v_bismark.txt', r"Bismark Version: v(\S+)"],
+    'R'       : ['v_R.txt', r"R version (\S+)"]
 }
 results = OrderedDict()
 results['nf-core/demultiplex'] = '<span style="color:#999999;\">N/A</span>'
 results['Nextflow'] = '<span style="color:#999999;\">N/A</span>'
 results['FastQC'] = '<span style="color:#999999;\">N/A</span>'
 results['MultiQC'] = '<span style="color:#999999;\">N/A</span>'
+results['Bismark'] = '<span style="color:#999999;\">N/A</span>'
+results['R'] = '<span style="color:#999999;\">N/A</span>'
 
 # Search each file using its regex
 for k, v in regexes.items():
